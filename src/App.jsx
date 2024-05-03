acb5542fdf813db446f26867dc18f033508d7581
danimisme <msubhanr53@gmail.com>
2024-05-04 00:34:03 +0700

    add navbar to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import "./App.css";
 import AboutSection from "./components/Fragments/AboutSection";
 import DottedBackground from "./components/Fragments/DottedBackground";
 import HeroSection from "./components/Fragments/HeroSection";
+import Navbar from "./components/Layout/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <DottedBackground>
         <HeroSection />
       </DottedBackground>
