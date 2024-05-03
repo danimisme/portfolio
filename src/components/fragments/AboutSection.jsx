9d9ebcb89e491ee132df79bfe825aaf7d0defb2f
danimisme <msubhanr53@gmail.com>
2024-05-03 22:43:38 +0700

    move TimeLine component to Element folder

---
+++ src/components/fragments/AboutSection.jsx
@@ -0,0 +1,30 @@
+import TimeLine from "../Elements/TimeLine";
+
+export default function AboutSection() {
+  return (
+    <section id="about" className="py-36">
+      <div className="container">
+        <div className="flex flex-wrap justify-center">
+          <div className="w-full px-4 mb-10  max-w-xl">
+            <h4 className="text-primary font-bold text-xl uppercase mb-3">
+              Tentang Saya
+            </h4>
+            <p className="text-base font-medium text-secondary ">
+              Perkenalkan nama saya Muhammad Subhan Ramdhani. Saya adalah
+              lulusan S1 Teknik Informatika Universitas Indraprasta PGRI. Saya
+              sangat tertarik untuk bekerja di bidang IT. Saya senang
+              mempelajari hal baru. Saya dapat beradaptasi dengan cepat. Saya
+              selalu berkomitmen untuk menyelesaikan tugas yang telah diberikan.
+            </p>
+          </div>
+          <div className="w-full px-4 max-w-xl ">
+            <h3 className="text-primary font-bold text-xl uppercase mb-3">
+              Pendidikan Terakhir
+            </h3>
+            <TimeLine />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
