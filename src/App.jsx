b40f4c5e3c0e9acefbb8f1080e15935bb7c4dfa3
danimisme <msubhanr53@gmail.com>
2024-05-03 16:51:47 +0700

    add hero section to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import HeroSection from "./components/fragments/HeroSection";
 
 function App() {
   return (
     <>
       <div>
-        <h1>Hello World</h1>
+        <HeroSection />
       </div>
     </>
   );
