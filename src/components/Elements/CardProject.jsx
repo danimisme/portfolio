f90a8e8f72fc2fe3cb22f82590921b8554916f98
danimisme <msubhanr53@gmail.com>
2024-05-05 15:15:38 +0700

    create CardProject element for portfolio

---
+++ src/components/Elements/CardProject.jsx
@@ -0,0 +1,15 @@
+export default function CardProject({ title, desc, img }) {
+  return (
+    <div className="mb-12 p-4 md:w-1/2 lg:w-1/3">
+      <div className="rounded-md shadow-md overflow-hidden">
+        <img
+          src={img}
+          alt="portfolio-image"
+          className="w-full aspect-[16/9] object-cover object-top "
+        />
+      </div>
+      <h3 className="font-semibold text-xl text-dark  mt-5 mb-3">{title}</h3>
+      <p className="font-medium text-base text-secondary">{desc}</p>
+    </div>
+  );
+}
