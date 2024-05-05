5e4783ccb6de730b9b7ece5958dc4b89e4a8090d
danimisme <msubhanr53@gmail.com>
2024-05-05 15:16:56 +0700

    add cardproject element to portfolio section

--- src/components/fragments/PortFolioSection.jsx
+++ src/components/fragments/PortFolioSection.jsx
@@ -1,10 +1,12 @@
+import CardProject from "../Elements/CardProject";
+
 export default function PortFolioSection() {
   return (
     <section id="portfolio" className="pt-36 pb-16 bg-slate-200">
       <div className="container">
         <div className="w-full px-4">
           <div className="max-w-xl mx-auto text-center mb-16">
-            <h4 className="text-primary font-semibold text-lg md:text-xl  mb-3">
+            <h4 className="text-primary font-semibold text-lg md:text-xl mb-3">
               Portfolio
             </h4>
             <h2 className="text-dark font-bold text-3xl mb-4">
@@ -16,6 +18,28 @@ export default function PortFolioSection() {
               illum, cum cumque aut expedita incidunt.
             </p>
           </div>
+        </div>
+        <div className="w-full px-4 flex flex-wrap justify-center">
+          <CardProject
+            title="Kelana Travel Web"
+            desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quos in doloremque non voluptate hic at laboriosam incidunt excepturi adipisci."
+            img="./portfolio-images/kelana.png"
+          />
+          <CardProject
+            title="Dibimbing food"
+            desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quos in doloremque non voluptate hic at laboriosam incidunt excepturi adipisci."
+            img="./portfolio-images/dibimbing-food.png"
+          />
+          <CardProject
+            title="Diminum Caffe"
+            desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quos in doloremque non voluptate hic at laboriosam incidunt excepturi adipisci."
+            img="./portfolio-images/diminum-caffe.png"
+          />
+          <CardProject
+            title="Bookshelf Apps Web"
+            desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quos in doloremque non voluptate hic at laboriosam incidunt excepturi adipisci."
+            img="./portfolio-images/bookshelf-app.png"
+          />
         </div>
       </div>
     </section>
