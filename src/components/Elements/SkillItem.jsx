d78c77c57829ba42023289dc269026759a236470
danimisme <msubhanr53@gmail.com>
2024-05-06 01:17:37 +0700

    add animate on hover

--- src/components/Elements/SkillItem.jsx
+++ src/components/Elements/SkillItem.jsx
@@ -1,10 +1,12 @@
 export default function SkillItem({ title, icon }) {
   return (
-    <div className="flex flex-col items-center mb-10 md:w-1/4 w-1/3">
-      <div className="w-20 h-20">
+    <div className="flex flex-col items-center mb-10 md:w-1/4 w-1/3 group">
+      <div className="w-20 h-20 group-hover:animate-bounce duration-300">
         <img src={icon} alt="" />
       </div>
-      <p className="text-lg font-medium text-dark text-center mt-3">{title}</p>
+      <p className="text-lg font-semibold text-dark text-center mt-3 cursor-context-menu group-hover:font-bold group-hover:text-xl">
+        {title}
+      </p>
     </div>
   );
 }
