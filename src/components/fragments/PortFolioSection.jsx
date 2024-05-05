39a83917f5eec4900b0eee5f963c772dfcdeeb8c
danimisme <msubhanr53@gmail.com>
2024-05-05 14:57:53 +0700

    create portfolio section

---
+++ src/components/fragments/PortFolioSection.jsx
@@ -0,0 +1,23 @@
+export default function PortFolioSection() {
+  return (
+    <section id="portfolio" className="pt-36 pb-16 bg-slate-200">
+      <div className="container">
+        <div className="w-full px-4">
+          <div className="max-w-xl mx-auto text-center mb-16">
+            <h4 className="text-primary font-semibold text-lg md:text-xl  mb-3">
+              Portfolio
+            </h4>
+            <h2 className="text-dark font-bold text-3xl mb-4">
+              Project Terbaru
+            </h2>
+            <p className="text-medium font-medium text-secondary">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Explicabo voluptate consequuntur tenetur similique dignissimos
+              illum, cum cumque aut expedita incidunt.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
