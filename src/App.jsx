dfbcfd18d493ef7d6281a7d6e9d6c2df16fde74d
danimisme <msubhanr53@gmail.com>
2024-05-05 17:02:17 +0700

    fix import skill section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
-import SkillSection from "./components/Fragments/SkillSection";
 import AboutSection from "./components/Fragments/AboutSection";
 import DottedBackground from "./components/Fragments/DottedBackground";
 import HeroSection from "./components/Fragments/HeroSection";
 import PortFolioSection from "./components/Fragments/PortFolioSection";
+import SkillSection from "./components/Fragments/SkillSection";
 import Navbar from "./components/Layout/Navbar";
 
 function App() {
