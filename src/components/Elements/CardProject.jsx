9c6f8fe3498415357dc778a04192ec62fb49f537
danimisme <msubhanr53@gmail.com>
2024-05-06 00:55:16 +0700

    add tect stack

--- src/components/Elements/CardProject.jsx
+++ src/components/Elements/CardProject.jsx
@@ -1,14 +1,32 @@
-export default function CardProject({ title, desc, img }) {
+export default function CardProject({ title, desc, img, techStack }) {
   return (
-    <div className="mb-12 p-4 md:w-1/2 lg:w-1/3">
-      <div className="rounded-md shadow-md overflow-hidden">
+    <div className="mb-12 p-3 md:p-6 md:w-1/2 lg:w-1/3 group hover:cursor-pointer hover:shadow-lg hover:bg-slate-300 hover:-translate-y-2 duration-300">
+      <div className="rounded-md shadow-md overflow-hidden relative">
         <img
           src={img}
           alt="portfolio-image"
-          className="w-full aspect-[16/9] object-cover object-top transition-all duration-1000 hover:object-bottom brightness-75 hover:brightness-100"
+          className="w-full aspect-[16/9] object-cover object-top transition-all duration-1000 group-hover:object-bottom brightness-100 group-hover:brightness-75 "
         />
+        <button className="font-semibold border-[3px] border-primary bg-black bg-opacity-20 py-2 px-5 rounded-lg text-primary absolute top-1/2 left-1/2 -translate-x-1/2 group-hover:-translate-y-1/2 opacity-0 group-hover:opacity-100 transition duration-1000 hover:bg-white hover:bg-opacity-50">
+          View
+        </button>
       </div>
       <h3 className="font-semibold text-xl text-dark  mt-5 mb-3">{title}</h3>
+      <h2 className="font-semibold text-base text-secondary">
+        Tech Stack :
+        {techStack && (
+          <span className="inline-block ml-3 mb-3">
+            {techStack.map((tech, index) => (
+              <img
+                key={index}
+                src={tech}
+                alt={tech}
+                className="h-7 inline-block mr-2"
+              />
+            ))}
+          </span>
+        )}
+      </h2>
       <p className="font-medium text-base text-secondary">{desc}</p>
     </div>
   );
