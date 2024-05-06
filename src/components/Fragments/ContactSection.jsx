b58bd8f5fdf18feea672fcc34b1727ea42e871aa
danimisme <msubhanr53@gmail.com>
2024-05-07 00:10:48 +0700

    modif style of contact section when darkmode

--- src/components/Fragments/ContactSection.jsx
+++ src/components/Fragments/ContactSection.jsx
@@ -5,7 +5,7 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export default function ContactSection() {
   return (
-    <section id="contact" className="py-32">
+    <section id="contact" className="py-32 dark:bg-dark">
       <div className="container">
         <div className="w-full px-4">
           <h2 className="font-bold text-primary text-3xl my-10 text-center lg:text-start">
@@ -14,17 +14,17 @@ export default function ContactSection() {
         </div>
         <div className="flex flex-wrap ">
           <div className="w-full md:w-2/3 lg:w-[40%] mx-auto lg:mx-0 px-4">
-            <h3 className="text-dark font-bold text-xl md:text-2xl mb-8">
+            <h3 className="text-dark font-bold text-xl md:text-2xl mb-8 dark:text-white">
               Mari berdikusi
             </h3>
-            <div className=" mt-4">
-              <div className="mb-3">
+            <div className=" mt-4 ">
+              <div className="mb-3 ">
                 <a
                   target="_blank"
                   href="https://api.whatsapp.com/send?phone=628979761661"
-                  className="text-md  group "
+                  className="text-md  group  dark:text-slate-300 "
                 >
-                  <span className="inline-block mr-3 text-secondary text-2xl group-hover:-translate-y-2 transition duration-300 ">
+                  <span className="inline-block mr-3 text-secondary  dark:text-slate-300 text-2xl group-hover:-translate-y-2 transition duration-300 ">
                     <FontAwesomeIcon icon={faWhatsapp} />
                   </span>
                   08979761661
@@ -34,9 +34,9 @@ export default function ContactSection() {
                 <a
                   target="_blank"
                   href="mailto:msubhanr53@gmail"
-                  className="text-md  group "
+                  className="text-md  group  dark:text-slate-300 "
                 >
-                  <span className="inline-block mr-3 text-secondary text-2xl group-hover:-translate-y-2 transition duration-300 ">
+                  <span className="inline-block mr-3 text-secondary  dark:text-slate-300 text-2xl group-hover:-translate-y-2 transition duration-300 ">
                     <FontAwesomeIcon icon={faEnvelope} />
                   </span>
                   msubhanr53@gmail.com
@@ -46,9 +46,9 @@ export default function ContactSection() {
                 <a
                   target="_blank"
                   href="https://www.linkedin.com/in/msubhanr/"
-                  className="text-md  group "
+                  className="text-md  group  dark:text-slate-300"
                 >
-                  <span className="inline-block mr-3 text-secondary text-2xl group-hover:-translate-y-2 transition duration-300 ">
+                  <span className="inline-block mr-3 text-secondary  dark:text-slate-300 text-2xl group-hover:-translate-y-2 transition duration-300 ">
                     <FontAwesomeIcon icon={faLinkedin} />
                   </span>
                   Muhammad Subhan Ramdhani
@@ -57,11 +57,13 @@ export default function ContactSection() {
             </div>
           </div>
           <div className="w-full my-10 lg:my-0 md:w-2/3 lg:w-[20%] mx-auto lg:mx-0 px-4">
-            <h3 className="text-dark font-bold text-xl md:text-2xl  ">Atau</h3>
+            <h3 className="text-dark font-bold text-xl md:text-2xl  dark:text-white ">
+              Atau
+            </h3>
           </div>
 
           <div className="w-full md:w-2/3 lg:w-[40%] mx-auto lg:mx-0 px-4">
-            <h3 className="text-dark font-bold text-xl md:text-2xl mb-8">
+            <h3 className="text-dark font-bold text-xl md:text-2xl mb-8  dark:text-white">
               Kirimikan Pesan
             </h3>
             <form action="">
