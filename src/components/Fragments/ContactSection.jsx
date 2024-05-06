f1ef4d4c00a6dd4228d14bf2163f6de964a26906
danimisme <msubhanr53@gmail.com>
2024-05-06 17:39:53 +0700

    deleting unused import icon

--- src/components/Fragments/ContactSection.jsx
+++ src/components/Fragments/ContactSection.jsx
@@ -1,12 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import InputForm from "../Elements/Input";
-import {
-  faFacebook,
-  faGithub,
-  faInstagram,
-  faLinkedin,
-  faWhatsapp,
-} from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export default function ContactSection() {
