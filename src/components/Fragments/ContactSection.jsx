e85be9c074a95aa718cb7413cd30f7937d7cf287
danimisme <msubhanr53@gmail.com>
2024-05-06 13:38:33 +0700

    add form contact

--- src/components/Fragments/ContactSection.jsx
+++ src/components/Fragments/ContactSection.jsx
@@ -1,9 +1,11 @@
+import InputForm from "../Elements/Input";
+
 export default function ContactSection() {
   return (
     <section id="contact" className="pt-36 pb-32">
       <div className="container">
         <div className="w-full px-4">
-          <div className="wax-w-xl mx-auto text-center mb-16">
+          <div className="max-w-xl mx-auto text-center mb-16">
             <h4 className=" text-primary font-semibold text-lg md:text-xl mb-2">
               Contact
             </h4>
@@ -17,7 +19,26 @@ export default function ContactSection() {
             </p>
           </div>
         </div>
-        <form action=""></form>
+        <div className="w-full md:w-2/3 mx-auto">
+          <form action="">
+            <InputForm name="name" label="Name" placeholder=" " />
+            <InputForm
+              type="email"
+              name="email"
+              label="Email"
+              placeholder=" "
+            />
+            <InputForm
+              type="textarea"
+              name="message"
+              label="Message"
+              placeholder=" "
+            />
+            <button className="bg-primary text-white px-5 py-3 rounded-lg mx-4 hover:opacity-80 hover:shadow-md transition duration-300">
+              Kirim
+            </button>
+          </form>
+        </div>
       </div>
     </section>
   );
