139e275199782f08e41a5fdad1e7fc3b0479bbb2
danimisme <msubhanr53@gmail.com>
2024-05-06 13:38:02 +0700

    create Input Form Element

---
+++ src/components/Elements/Input/index.jsx
@@ -0,0 +1,16 @@
+import Input from "./Input";
+import Label from "./Label";
+import TextArea from "./TextArea";
+
+export default function InputForm({ type, name, label, ...props }) {
+  return (
+    <div className="w-full relative z-0 mx-4 mb-8">
+      {type === "textarea" ? (
+        <TextArea name={name} {...props} />
+      ) : (
+        <Input type={type} name={name} {...props} />
+      )}
+      <Label name={name} label={label} />
+    </div>
+  );
+}
