6d224818389b0797f576476318a12b818d64d3cd
danimisme <msubhanr53@gmail.com>
2024-05-07 00:12:28 +0700

    move Footer to Layout folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import "./App.css";
 import ButtonUp from "./components/Elements/ButtonUp";
 import AboutSection from "./components/Fragments/AboutSection";
 import ContactSection from "./components/Fragments/ContactSection";
-import Footer from "./components/Fragments/Footer";
 import HeroSection from "./components/Fragments/HeroSection";
 import PortFolioSection from "./components/Fragments/PortFolioSection";
 import SkillSection from "./components/Fragments/SkillSection";
 import FloatingBackgrund from "./components/Layout/FloatingBackgrund";
+import Footer from "./components/Layout/Footer";
 import Navbar from "./components/Layout/Navbar";
 
 function App() {
