ff3bb57e581283d7b72a9533f8b1b206c19299b1
danimisme <msubhanr53@gmail.com>
2024-05-06 13:37:35 +0700

    create TextArea Element

---
+++ src/components/Elements/Input/TextArea.jsx
@@ -0,0 +1,11 @@
+export default function TextArea({ name, ...props }) {
+  return (
+    <textarea
+      id={name}
+      name={name}
+      className="w-full mt-2 text-secondary p-3 border-b-[3px] border-secondary outline-none focus:border-primary focus:shadow-md focus:invalid:border-red-500 h-32 block py-2.5 px-0 text-s bg-transparent border-0  appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 peer"
+      {...props}
+      required
+    ></textarea>
+  );
+}
