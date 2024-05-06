72222256713e0d9d6f961fa5c25d8fe9e4c6d459
danimisme <msubhanr53@gmail.com>
2024-05-06 14:20:28 +0700

    create footer component

---
+++ src/components/Fragments/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer className="bg-dark pt-24 pb-12">
+      <div className="container">
+        <div className="flex flex-wrap">
+          <div className="w-full px-4 mb-12">
+            <p>© Copyright 2024. danimisme built by Muhammad Subhan Ramdhani</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
