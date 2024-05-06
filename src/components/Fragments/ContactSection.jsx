b45aedc77849be375189ca5bfac976ac44b78ee8
danimisme <msubhanr53@gmail.com>
2024-05-06 12:39:01 +0700

    create contact section component

---
+++ src/components/Fragments/ContactSection.jsx
@@ -0,0 +1,24 @@
+export default function ContactSection() {
+  return (
+    <section id="contact" className="pt-36 pb-32">
+      <div className="container">
+        <div className="w-full px-4">
+          <div className="wax-w-xl mx-auto text-center mb-16">
+            <h4 className=" text-primary font-semibold text-lg md:text-xl mb-2">
+              Contact
+            </h4>
+            <h2 className="font-bold text-dark text-3xl md:text-4xl mb-4">
+              Hubungi Saya
+            </h2>
+            <p className="text-md font-medium text-secondary">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. A et
+              aliquid possimus iure corrupti obcaecati quod sunt eligendi
+              tenetur non!
+            </p>
+          </div>
+        </div>
+        <form action=""></form>
+      </div>
+    </section>
+  );
+}
