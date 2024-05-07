29c39800d02d81b8fb847a8810c31f49cd4af9f0
danimisme <msubhanr53@gmail.com>
2024-05-07 19:16:52 +0700

    add some data project to show

--- src/components/Fragments/DetailProjectModal.jsx
+++ src/components/Fragments/DetailProjectModal.jsx
@@ -1,13 +1,45 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { closeProjectModal } from "../../redux/slices/ProjectModalSlice";
+import { FaGithub, FaLink } from "react-icons/fa";
+import { useEffect, useState } from "react";
 
 export default function DetailProjectModal() {
+  const { isOpen, project } = useSelector((state) => state.projectModal);
+  const dispatch = useDispatch();
+  const { img, title, techStack, desc, repository, link } = project;
+  const [width, setWidth] = useState(getWidth());
+  function getWidth() {
+    const width = window.innerWidth;
+    if (width <= 768) {
+      return 1;
+    } else if (width <= 1024) {
+      return 2;
+    } else {
+      return 3;
+    }
+  }
+  useEffect(() => {
+    function handleResize() {
+      setWidth(getWidth());
+    }
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div className="fixed  top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50 ">
-      <div className=" bg-white dark:bg-dark dark:bg-opacity-70 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-4 w-[80%] lg:w-[70%]  rounded-lg bg-opacity-70 overflow-y-auto">
+    <div
+      className={`fixed h-screen top-0 left-0 w-full  flex justify-center items-center bg-black bg-opacity-50 z-50 transition-all duration-500 ${isOpen ? "opacity-100 pointer-events-auto " : " opacity-0 pointer-events-none"}`}
+    >
+      <div
+        className={` bg-white backdrop-blur-sm bg-opacity-70 dark:bg-dark dark:bg-opacity-70 dark:backdrop-blur-sm absolute p-4 w-[80%] lg:w-[70%] max-h-[90vh] rounded-lg  overflow-y-auto transition-all duration-500 ${isOpen ? "opacity-100 translate-y-0" : "translate-y-24 opacity-0 "}`}
+      >
         <div className="w-full border-b-2 border-dark dark:text-white dark:border-slate-500 py-2 ">
-          <h1 className="text-2xl font-semibold text-center mb-2 ">
-            Kelana Travel
+          <h1 className="text-xl lg:text-2xl font-semibold text-center mb-2 ">
+            {title}
           </h1>
         </div>
         <div className="flex flex-wrap  pt-10">
@@ -16,30 +48,94 @@ export default function DetailProjectModal() {
               showArrows={true}
               showStatus={false}
               showIndicators={false}
+              showThumbs={width > 1}
+              swipeable={true}
+              emulateTouch={true}
             >
-              <div className="w-full rounded-lg overflow-hidden">
-                <img
-                  src="https://source.unsplash.com/random"
-                  alt=""
-                  className="aspect-[16/9] object-top object-cover w-full"
-                />
-              </div>
+              {img &&
+                img.map((image, index) => (
+                  <div
+                    key={index}
+                    className="w-full rounded-lg overflow-hidden"
+                  >
+                    <img
+                      src={image}
+                      alt=""
+                      className="w-full aspect-[16/9] object-cover object-top"
+                    />
+                  </div>
+                ))}
             </Carousel>
           </div>
           <div className="w-full md:w-1/2 px-4 dark:text-white">
-            <h2 className="text-xl font-semibold mb-4">Description</h2>
-            <p>
-              Memanfaatkan data dari travel API dibimbing.id, admin dapat
-              mengakses dashboard dan dapat melakukan CRUD data promo, activty,
-              banner,profile dan mengupdate role user.
+            <h2 className="text-lg lg:text-xl font-semibold my-2 lg:mb-4">
+              Description
+            </h2>
+            <p className="text-sm md:text-base">{desc}</p>
+            <p className="text-base lg:text-xl font-semibold mt-4 mb-4">
+              Tech Stack :
+              {techStack && (
+                <span className="inline-block ml-3 mb-3">
+                  {techStack.map((tech, index) => (
+                    <img
+                      key={index}
+                      src={tech}
+                      alt={tech}
+                      className="h-5 lg:h-7 inline-block mr-2 mt-2"
+                    />
+                  ))}
+                </span>
+              )}
             </p>
-            <h2 className="text-xl font-semibold mt-4 mb-4">Tech Stack</h2>
-            <ul>
-              <li>React</li>
-              <li>Redux</li>
-              <li>React-Query</li>
-              <li>Tailwind</li>
-            </ul>
+            <div className="flex lg:flex-col gap-4">
+              <div className="relative group">
+                <a
+                  href={repository}
+                  className="text-sm lg:text-base font-semibold hover:text-primary"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <span className="inline-block mr-2 text-lg group-hover:animate-bounce transition duration-300">
+                    <FaGithub />
+                  </span>
+                  Repository
+                </a>
+                <div className="absolute -bottom-8 left-10 py-1 px-2 h-7 rounded-lg bg-white dark:bg-dark dark:text-white hidden group-hover:block z-10 ">
+                  Click to open
+                </div>
+              </div>
+              <div className="relative  group">
+                <a
+                  href={link}
+                  className="text-sm lg:text-base font-semibold hover:text-primary"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <span className="inline-block mr-2 text-lg group-hover:animate-bounce transition duration-300">
+                    <FaLink />
+                  </span>
+                  Link Demo
+                </a>
+                <div className="absolute -bottom-8 left-10 py-1 px-2 h-7 rounded-lg bg-white dark:bg-dark dark:text-white hidden group-hover:block z-10">
+                  Click to open
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex gap-4 justify-center w-full mt-4">
+            <a
+              href={link}
+              target="_blank"
+              className="border-2 border-primary text-primary px-4 py-2 rounded-lg hover:bg-primary hover:text-white transition duration-300"
+            >
+              Visit
+            </a>
+            <button
+              className="border-2 border-red-500 text-red-500 px-4 py-2 rounded-lg hover:bg-red-500 hover:text-white transition duration-300"
+              onClick={() => dispatch(closeProjectModal())}
+            >
+              Close
+            </button>
           </div>
         </div>
       </div>
