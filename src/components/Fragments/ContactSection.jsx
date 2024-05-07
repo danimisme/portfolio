dcf11505b25dee109fcc2ac822fa0ed0a2a909ca
danimisme <msubhanr53@gmail.com>
2024-05-08 00:46:48 +0700

    add data aos for contact section

--- src/components/Fragments/ContactSection.jsx
+++ src/components/Fragments/ContactSection.jsx
@@ -2,18 +2,26 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import InputForm from "../Elements/Input";
 import { faLinkedin, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { useEffect } from "react";
+import animationAOS from "../../AOS/setting";
 
 export default function ContactSection() {
+  useEffect(() => {
+    animationAOS();
+  }, []);
   return (
     <section id="contact" className="py-32 dark:bg-dark">
       <div className="container">
-        <div className="w-full px-4">
+        <div className="w-full px-4" data-aos="fade-right">
           <h2 className="font-bold text-primary text-3xl my-10 text-center lg:text-start">
             Contact
           </h2>
         </div>
         <div className="flex flex-wrap ">
-          <div className="w-full md:w-2/3 lg:w-[40%] mx-auto lg:mx-0 px-4">
+          <div
+            className="w-full md:w-2/3 lg:w-[40%] mx-auto lg:mx-0 px-4"
+            data-aos="fade-right"
+          >
             <h3 className="text-dark font-bold text-xl md:text-2xl mb-8 dark:text-white">
               Mari berdikusi
             </h3>
@@ -56,13 +64,21 @@ export default function ContactSection() {
               </div>
             </div>
           </div>
-          <div className="w-full my-10 lg:my-0 md:w-2/3 lg:w-[20%] mx-auto lg:mx-0 px-4">
+          <div
+            className="w-full my-10 lg:my-0 md:w-2/3 lg:w-[20%] mx-auto lg:mx-0 px-4"
+            data-aos="fade-left"
+            data-aos-duration="500"
+          >
             <h3 className="text-dark font-bold text-xl md:text-2xl  dark:text-white ">
               Atau
             </h3>
           </div>
 
-          <div className="w-full md:w-2/3 lg:w-[40%] mx-auto lg:mx-0 px-4">
+          <div
+            className="w-full md:w-2/3 lg:w-[40%] mx-auto lg:mx-0 px-4"
+            data-aos="fade-left"
+            data-aos-duration="500"
+          >
             <h3 className="text-dark font-bold text-xl md:text-2xl mb-8  dark:text-white">
               Kirimikan Pesan
             </h3>
