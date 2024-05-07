f21e933a2f1f7852d95387e2eb3efad4046fa0d7
danimisme <msubhanr53@gmail.com>
2024-05-07 14:37:18 +0700

    using data projects for card project

--- src/components/Fragments/PortFolioSection.jsx
+++ src/components/Fragments/PortFolioSection.jsx
@@ -1,6 +1,22 @@
 import CardProject from "../Elements/CardProject";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function PortFolioSection() {
+  const [projects, setProjects] = useState([]);
+  useEffect(() => {
+    fetchProjects();
+  }, []);
+  const fetchProjects = async () => {
+    try {
+      const response = await fetch("/src/data/projects.JSON");
+      const data = await response.json();
+      console.log(data);
+      setProjects(data);
+    } catch (error) {
+      console.error("Error fetching projects:", error);
+    }
+  };
   return (
     <section
       id="portfolio"
@@ -22,43 +38,15 @@ export default function PortFolioSection() {
           </div>
         </div>
         <div className="w-full px-4 flex flex-wrap justify-center">
-          <CardProject
-            title="Kelana Travel Web"
-            techStack={[
-              "./icons/reactjs.png",
-              "./icons/tailwindcss.png",
-              "./icons/redux.png",
-              "./icons/axios.png",
-            ]}
-            desc="Memanfaatkan data dari travel API dibimbing.id, admin dapat mengakses dashboard dan dapat melakukan CRUD data promo, activty, banner,profile dan mengupdate role user."
-            img="./portfolio-images/kelana.png"
-          />
-          <CardProject
-            title="Dibimbing food"
-            techStack={[
-              "./icons/nextjs.png",
-              "./icons/tailwindcss.png",
-              "./icons/axios.png",
-            ]}
-            desc="Sebuah website food yang mengambil data dari food API dibimbing.id . user dapat melakukan CRUD menu food."
-            img="./portfolio-images/dibimbing-food.png"
-          />
-          <CardProject
-            title="Diminum Caffe"
-            techStack={["./icons/reactjs.png", "./icons/bootstrap.png"]}
-            desc="Sebuah website landing page diminum caffe. menampilkan homepage diminum caffe, tentang diminum caffe, dan menu diminum caffe."
-            img="./portfolio-images/diminum-caffe.png"
-          />
-          <CardProject
-            title="Bookshelf Apps Web"
-            techStack={[
-              "./icons/html.png",
-              "./icons/css.png",
-              "./icons/js.png",
-            ]}
-            desc="Memanfaatkan local Storage untuk menyimpan data. user dapat menambahkan buku, mengedit buku, dan menghapus buku."
-            img="./portfolio-images/bookshelf-app.png"
-          />
+          {projects.map((project, index) => (
+            <CardProject
+              key={index}
+              title={project.title}
+              desc={project.desc}
+              img={project.img}
+              techStack={project.techStack}
+            />
+          ))}
         </div>
       </div>
     </section>
