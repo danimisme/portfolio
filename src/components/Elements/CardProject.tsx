e94633230432cc28801971d9a0e84447006a93ad
danimisme <msubhanr53@gmail.com>
2025-07-22 00:21:58 +0700

    fix color

--- src/components/Elements/CardProject.tsx
+++ src/components/Elements/CardProject.tsx
@@ -27,7 +27,7 @@ export default function CardProject({ project } : Props) {
           alt="portfolio-image"
           className="w-full aspect-[16/9] object-cover object-top transition-all duration-1000 group-hover:object-bottom brightness-100 group-hover:brightness-75 "
         />
-        <button className="font-semibold border-[3px] border-primary bg-black bg-opacity-20 py-2 px-5 rounded-lg text-primary absolute top-1/2 left-1/2 -translate-x-1/2 group-hover:-translate-y-1/2 opacity-0 group-hover:opacity-100 transition duration-1000 hover:bg-white hover:bg-opacity-50">
+        <button className="font-semibold border-[3px] border-primary bg-black/20 py-2 px-5 rounded-lg text-primary absolute top-1/2 left-1/2 -translate-x-1/2 group-hover:-translate-y-1/2 opacity-0 group-hover:opacity-100 transition duration-1000 hover:bg-white hover:bg-opacity-50">
           View
         </button>
       </div>
