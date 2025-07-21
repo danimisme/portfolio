ba4ca1d663e2998235793c4c18c6340a2604d157
danimisme <msubhanr53@gmail.com>
2025-07-21 19:09:41 +0700

    add after component for active article

--- src/app/article/ArticleList.tsx
+++ src/app/article/ArticleList.tsx
@@ -13,7 +13,15 @@ export default function ArticleList({ articles }: Props) {
       {articles.map((art) => (
         <li key={art.slug} className="mb-1">
           <Link href={`/article/${art.slug}`}>
-            <p className={`text-gray-600 dark:text-gray-400 hover:text-gray-800 ${pathName === `/article/${art.slug}` ? "font-bold" : ""}`}>
+            <p
+              className={`
+                relative article-item mb-1
+                text-gray-600 dark:text-gray-400
+                hover:text-gray-800 dark:hover:text-blue-400
+                transition-colors
+                ${pathName === `/article/${art.slug}` ? "font-bold active" : ""}
+              `}
+            >
               {art.title}
             </p>
           </Link>
--- src/app/article/Sidebar.tsx
+++ src/app/article/Sidebar.tsx
@@ -32,12 +32,12 @@ export default function Sidebar({ articles }: SidebarProps) {
       <aside
         className={`
           font-poppins text-sm h-full md:h-auto sm:pt-16 md:pt-0 px-4 w-64 bg-gray-100 dark:bg-darkgray border-r dark:border-gray-600
-          fixed top-0 left-0 z-20 transition-transform duration-300 ease-in-out
+          fixed top-0 left-0 z-20 transition-transform duration-700 ease-in-out
           ${isOpen ? "translate-x-0" : "-translate-x-full"}
            md:relative md:z-0 ${isOpen ? "md:w-64" : "md:w-0"}
         `}
       >
-        <div className={`mt-4 md:mt-8 lg:mt-12 sticky top-16 ${isOpen ? "block" : "hidden"}`}>
+        <div className={`mt-4 md:mt-8 lg:mt-12 sticky top-16 ${isOpen ? "opacity-100" : "opacity-0"}`}>
           <div className="flex items-center justify-between mb-4">
             <h2 className="font-bold dark:text-white">List Artikel</h2>
             {isOpen && (
