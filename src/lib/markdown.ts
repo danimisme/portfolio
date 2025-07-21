a07d1579ed398525fc1fc98a31a14385191627e1
danimisme <msubhanr53@gmail.com>
2025-07-21 16:59:41 +0700

    create markdown lib for reading file markdown from system

---
+++ src/lib/markdown.ts
@@ -0,0 +1,36 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import { ArticleMeta } from '@/types/Article';
+
+const articlesDir = path.join(process.cwd(), 'public/article');
+const notFoundPath = path.join(process.cwd(), 'public/not-found');
+
+export function getArticleSlugs(): string[] {
+  return fs
+    .readdirSync(articlesDir)
+    .filter((f) => f.endsWith('.md'))
+    .map((f) => f.replace(/\.md$/, ''));
+}
+
+export function getAllArticlesMeta(): ArticleMeta[] {
+  return getArticleSlugs().map((slug) => {
+    const fullPath = path.join(articlesDir, `${slug}.md`);
+    const { data } = matter(fs.readFileSync(fullPath, 'utf8'));
+    return {
+      slug,
+      title: data.title || slug,
+    };
+  });
+}
+
+export function getArticleRaw(slug: string): string {
+  const fullPath = path.join(articlesDir, `${slug}.md`);
+  try {
+    const data = fs.readFileSync(fullPath, 'utf8');
+    return data
+  } catch (e) {
+    return fs.readFileSync(path.join(notFoundPath, '404.md'), 'utf8');
+  }
+  
+}
